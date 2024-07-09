c5693d326f99a92c49db64ef8526bc98f6d429e9
kageruka02 <leonkageruka@gmail.com>
2024-07-09 16:51:20 +0300

    define routes which do not need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,7 +5,7 @@ from flask import request
 from typing import List, TypeVar
 
 
-class Auth():
+class Auth:
     """manages the API authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
