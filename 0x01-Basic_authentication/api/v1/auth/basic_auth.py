98e2d69dca055c13d29896babb5c284ac38111ce
kageruka02 <leonkageruka@gmail.com>
2024-07-09 23:35:25 +0300

    returns the user email and password from the Base64 decoded value.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,14 +3,15 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
     """BasicAuth Class"""
 
     def extract_base64_authorization_header(
-            self, authorization_header: str
-            ) -> str:
+        self, authorization_header: str
+    ) -> str:
         """Returns the Base64 part of the Authorization header"""
         if not isinstance(authorization_header, str):
             return None
@@ -27,9 +28,22 @@ class BasicAuth(Auth):
             not isinstance(base64_authorization_header, str)
             or base64_authorization_header is None
         ):
-            return
+            return None
         try:
             decoded_bytes = base64.b64decode(base64_authorization_header)
             return decoded_bytes.decode("utf-8")
         except base64.binascii.Error:
-            return
+            return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> Tuple[str, str]:
+        """Returns the user email and password from the Base64 decoded value"""
+        if (
+            not decoded_base64_authorization_header
+            or not isinstance(decoded_base64_authorization_header, str)
+            or ":" not in decoded_base64_authorization_header
+        ):
+            return None, None
+        extract = decoded_base64_authorization_header.split(":", 1)
+        return (extract[0], extract[1]) if extract else (None, None)
