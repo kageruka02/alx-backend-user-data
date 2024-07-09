536ce8d2c9f7ebf6a8f8436b37112732f7f05fe8
kageruka02 <leonkageruka@gmail.com>
2024-07-09 22:24:34 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,7 +19,7 @@ if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 
 if getenv("AUTH_TYPE") == "basic_auth":
-    from api.v1.auth.auth import BasicAuth
+    from api.v1.auth.basic_auth import BasicAuth
 
     auth = BasicAuth()
 
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,7 +29,3 @@ class Auth:
     def current_user(self, request=None) -> TypeVar("User"):
         """current user method"""
         return None
-
-
-class BasicAuth(Auth):
-    pass
