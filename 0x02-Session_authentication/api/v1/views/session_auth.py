fbea255f86e953d6f18a7b51fdbee79b3a9e2430
kageruka02 <leonkageruka@gmail.com>
2024-07-17 22:24:46 +0300

    login session authentications

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -7,7 +7,7 @@ from models.user import User
 from os import getenv
 
 
-@app_views("/auth_session/login", methods=["POST"], strict_slashes=False)
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
 def login():
     """POST /auth_session/login
     Return
