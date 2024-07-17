b87bb6209a07ad25ea88d7fd78ad640a4816af03
kageruka02 <leonkageruka@gmail.com>
2024-07-17 22:58:35 +0300

    logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -40,7 +40,11 @@ def login():
     return response
 
 
-@app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=False)
+@app_views.route(
+    "/auth_session/logout",
+    methods=["DELETE"],
+    strict_slashes=False
+)
 def logout():
     """DELETE /auth_session/logout
     Return:
