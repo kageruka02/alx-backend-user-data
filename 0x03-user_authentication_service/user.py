ed24436466ffacd93081fb052c43a0021507e7ed
kageruka02 <leonkageruka@gmail.com>
2024-07-22 00:12:27 +0300

    make sql alchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Integer, VARCHAR, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+"authentication service full one"
+
+Base = declarative_base()
+
+
+class User(Base):
+    "sql alchemy"
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
