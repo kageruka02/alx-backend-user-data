1c4e69a8e242121779a98ef90a9856be7a460d97
kageruka02 <leonkageruka@gmail.com>
2024-07-22 01:17:37 +0300

    create user without validation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -27,7 +27,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
-        user = User(email=email, hashed_password=hashed_password)
-        dbsession = sessionmaker()
-        session = dbsession
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user to the database"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
