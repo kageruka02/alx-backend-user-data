e1fb995475164a5681cb49aaf90f746d6ae2058c
kageruka02 <leonkageruka@gmail.com>
2024-07-22 13:55:34 +0300

    hashing a password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module for authentication.
+"""
+import bcrypt
+
+
+def _hash_password(password: str):
+    "hashing a password"
+    password = password.encode()
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password, salt)
